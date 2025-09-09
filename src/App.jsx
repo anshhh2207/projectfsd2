081d4792aede8d1fc24f8197cf397c0ee48af438
anshhh2207 <150226018+anshhh2207@users.noreply.github.com>
2025-09-09 12:12:30 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Login from './components/Login'
+import Signup from './components/Signup'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Routes>
+         <Route path ='/L' element={<Login/>}/>
+          <Route path ='/S' element={<Signup/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
